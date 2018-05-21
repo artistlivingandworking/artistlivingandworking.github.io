1ab457c3d10918041423b2e8b75b2a8cbae9b63c
Micha Gorelick <mynameisfiber@gmail.com>
2018-05-21 18:59:34 -0400

    new imgur albums show up in dance

--- assets/js/imgur.js
+++ assets/js/imgur.js
@@ -1,13 +1,19 @@
-async function imgurAlbumItems(album_hash) {
+var _imgur_clientID = '23524a5f57df19a';
+
+async function _imgurRequest(path) {
     var data = await $.ajax({
-        url: 'https://api.imgur.com/3/album/' + album_hash,
+        url: 'https://api.imgur.com/3/' + path,
         dataType: "json",
         crossDomain:true,
         headers: {
-            "Authorization": "Client-ID 23524a5f57df19a"
+            "Authorization": "Client-ID " + _imgur_clientID,
         }
     });
+    return data;
+}
+async function imgurAlbumItems(album_hash) {
     var items=[]
+    var data = await _imgurRequest('/album/' + album_hash);
     for (var i in data.data.images) {
         var url = data.data.images[i].link;
         var el= $("<figure class='image imgur column is-4'></figure>").append(
@@ -17,5 +23,11 @@ async function imgurAlbumItems(album_hash) {
         );
         items.push(el);
     }
-    return items;
+    data.items = items;
+    return data;
+}
+
+async function imgurAlbums(user) {
+    var data = await _imgurRequest('/account/' + user + '/albums');
+    return data['data'];
 }
