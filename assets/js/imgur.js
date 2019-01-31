5df4cfa414461c07df8d1d25d2f0fe5ce8d14e52
Micha Gorelick <mynameisfiber@gmail.com>
2019-01-31 14:21:55 -0500

    added credits to images, made modal view nicer, changed front page

--- assets/js/imgur.js
+++ assets/js/imgur.js
@@ -1,7 +1,7 @@
 var _imgur_clientID = '23524a5f57df19a';
 
 async function _imgurRequest(path) {
-    var data = await $.ajax({
+    let data = await $.ajax({
         url: 'https://api.imgur.com/3/' + path,
         dataType: "json",
         crossDomain:true,
@@ -9,25 +9,32 @@ async function _imgurRequest(path) {
             "Authorization": "Client-ID " + _imgur_clientID,
         }
     });
+    console.log(data);
     return data;
 }
 async function imgurAlbumItems(album_hash) {
-    var items=[]
-    var data = await _imgurRequest('/album/' + album_hash);
+    let items=[]
+    let data = await _imgurRequest('/album/' + album_hash);
     for (var i in data.data.images) {
-        var url = data.data.images[i].link;
-        var el= $("<figure class='image imgur column is-4'></figure>").append(
-            $("<a></a>").attr('href', url).append(
-                $("<img>").attr('src', url)
+      let url = data.data.images[i].link;
+      let desc = data.data.images[i].description || '';
+      let el= $("<div class='column is-4'></div>").append(
+        $("<figure class='image imgur'></figure>")
+          .append($("<a></a>").attr('href', url)
+            .append(
+                $("<figcaption>").text(desc)
+            ).append(
+              $("<img>").attr('src', url).attr('alt', desc)
             )
+          )
         );
-        items.push(el);
+      items.push(el);
     }
     data.items = items;
     return data;
 }
 
 async function imgurAlbums(user) {
-    var data = await _imgurRequest('/account/' + user + '/albums');
+    let data = await _imgurRequest('/account/' + user + '/albums');
     return data['data'];
 }
